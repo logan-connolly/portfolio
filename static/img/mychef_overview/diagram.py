2f257a96d52ca617866ad4f9b18f00e11073808d
Logan Connolly <logan.connolly@protonmail.com>
2020-10-02 09:07:27 +0200

    Simplify Mychef Overview post

--- static/img/mychef_overview/diagram.py
+++ static/img/mychef_overview/diagram.py
@@ -1,34 +1,38 @@
 from diagrams import Cluster, Diagram
-from diagrams.programming.language import Python
+from diagrams.programming.language import Python, Rust
 from diagrams.programming.framework import Vue
 from diagrams.onprem.compute import Server
 from diagrams.onprem.database import PostgreSQL
-from diagrams.onprem.network import Nginx
 
 graph_attr = {
     "bgcolor": "transparent"
 }
 
 with Diagram("Basic Architecture", outformat="png", graph_attr=graph_attr):
-    ingress = Nginx("Web Server")
 
-    with Cluster("Service Cluster"):
-        gcp = [
-            Server("GCP")
+    with Cluster("Docker Swarm"):
+        host = [
+            Server("Cloud Host")
         ]
 
     with Cluster("Frontend"):
-        frontend = Vue("Vue + Vuetify")
-        gcp >> frontend
+        frontend = Vue("Nuxt + Vuetify")
+        host << frontend
+
+    with Cluster("Search Engine"):
+        search = Rust("MeiliSearch")
+        frontend << search
 
     with Cluster("API"):
         api = Python("FastAPI")
-        frontend >> api
+        search >> api
+        api >> search
 
     with Cluster("Database"):
         db = PostgreSQL("Recipes")
         api >> db
+        db >> api
 
     with Cluster("Web Scraper"):
         scraper = Python("Scrapy")
-        ingress >> gcp >> scraper >> api
+        scraper >> api
